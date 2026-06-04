449bcd1c6fabfbf2e351b865edf2d29f0376ee4e
ABHI <abhisekprusty012@gmail.com>
2026-06-04 23:48:23 +0530

    Initial commit

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,66 @@
+import userModel from "../models/user.model.js";
+import crypto from "crypto";
+import jwt from "jsonwebtoken"
+import config from "../config/config.js";
+
+
+export async function register(req , res){
+
+    const {username, email, password} = req.body;
+
+    const isAlreadyRegistered = await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+
+    if(isAlreadyRegistered){
+        res.json(409).json({
+            message: "Username or email already exists"
+        })
+    }
+
+
+    const hashedPassword = crypto.createHash("sha256").update(password).digest("hex");
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hashedPassword
+    })
+
+
+    const token = jwt.sign({
+        id: user._id
+    },config.JWT_SECRET,{
+        expiresIn: "10d"
+    })
+
+    res.status(201).json({
+        message: "User registerd successfully",
+        user:{
+            username: user.username,
+            email:user.email,
+           
+
+        },token
+    })
+
+
+}
+
+export async function getMe(req,res){
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if(!token){
+        res.status(401).json({
+            message: "Token not found"
+        })
+    }
+
+    const decoded = jwt.verify(token,config.JWT_SECRET);
+    
+    const user
+
+}
